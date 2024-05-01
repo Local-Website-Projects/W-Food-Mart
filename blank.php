6f560f21076474fc2a36c0b23316d45ffec4696c
Mugdho <saha2171515@stud.kuet.ac.bd>
2024-05-01 18:45:02 +0600

    initial push

---
+++ blank.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8"/>
+    <title>Food Mart</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php include('include/css.php'); ?>
+</head>
+
+<body class="">
+<!-- Left Sidenav -->
+<?php include('include/leftSideBar.php'); ?>
+<!-- end left-sidenav-->
+
+
+<div class="page-wrapper">
+    <!-- Top Bar Start -->
+    <?php include('include/topBar.php'); ?>
+    <!-- Top Bar End -->
+
+    <!-- Page Content-->
+    <div class="page-content">
+        <div class="container-fluid">
+            <!-- Page-Title -->
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="page-title-box">
+                        <div class="row">
+                            <div class="col">
+                                <h4 class="page-title">Page Title</h4>
+                            </div><!--end col-->
+                        </div><!--end row-->
+                    </div><!--end page-title-box-->
+                </div><!--end col-->
+            </div>
+            <!--end row-->
+            <!-- end page title end breadcrumb -->
+
+
+
+        </div><!-- container -->
+
+        <?php include ('include/footer.php');?>
+        <!--end footer-->
+    </div>
+    <!-- end page content -->
+</div>
+<!-- end page-wrapper -->
+
+
+<!-- jQuery  -->
+<?php include('include/js.php'); ?>
+
+</body>
+
+</html>
---
+++ include/topBar.php
@@ -0,0 +1,29 @@
+<div class="topbar">
+    <!-- Navbar -->
+    <nav class="navbar-custom">
+        <ul class="list-unstyled topbar-nav float-end mb-0">
+            <li class="dropdown">
+                <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button"
+                   aria-haspopup="false" aria-expanded="false">
+                    <span class="ms-1 nav-user-name hidden-sm">Nick</span>
+                    <img src="assets/images/users/user-5.jpg" alt="profile-user" class="rounded-circle thumb-xs" />
+                </a>
+                <div class="dropdown-menu dropdown-menu-end">
+                    <a class='dropdown-item' href='pages-profile.html'><i data-feather="user" class="align-self-center icon-xs icon-dual me-1"></i> Profile</a>
+                    <a class='dropdown-item' href='apps-contact-list.html'><i data-feather="users" class="align-self-center icon-xs icon-dual me-1"></i> Contacts</a>
+                    <div class="dropdown-divider mb-0"></div>
+                    <a class='dropdown-item' href='auth-login.html'><i data-feather="power" class="align-self-center icon-xs icon-dual me-1"></i> Logout</a>
+                </div>
+            </li>
+        </ul><!--end topbar-nav-->
+
+        <ul class="list-unstyled topbar-nav mb-0">
+            <li>
+                <button class="nav-link button-menu-mobile">
+                    <i data-feather="menu" class="align-self-center topbar-icon"></i>
+                </button>
+            </li>
+        </ul>
+    </nav>
+    <!-- end navbar-->
+</div>
